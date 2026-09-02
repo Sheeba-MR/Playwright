b4ff99f377b7f4d03be6fbdae33cbfd65df46459
Sheeba-MR <sheebajebin23@gmail.com>
2026-09-02 11:13:53 +0530

    Add files via upload
    Week1 Day2 Home Assignment

---
+++ dataTypesHA.js
@@ -0,0 +1,15 @@
+let userName = 'Sheeba'
+console.log(userName)
+console.log(typeof userName)
+let organization = "Organization A"
+console.log(organization)
+console.log(typeof organization)
+let contactNumber = 8903126287
+console.log(contactNumber)
+console.log(typeof contactNumber)
+let knowsAutomation = true
+console.log(knowsAutomation)
+console.log(typeof knowsAutomation)
+let usesPlaywright
+console.log(usesPlaywright)
+console.log(typeof usesPlaywright)
